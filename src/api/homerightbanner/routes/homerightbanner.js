8bf11f54e616b3b1b4982c5f9df1b6e412960bba
aecgdh <86180934+aecgdh@users.noreply.github.com>
2023-09-14 14:20:54 +0530

    new

--- src/api/homerightbanner/routes/homerightbanner.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * homerightbanner router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::homerightbanner.homerightbanner');
--- src/api/homerightbanner/services/homerightbanner.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * homerightbanner service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::homerightbanner.homerightbanner');
