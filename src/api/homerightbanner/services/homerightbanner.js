2346eafed047969382dd7703b8485cdbe0ffd742
aecgdh <86180934+aecgdh@users.noreply.github.com>
2023-09-21 12:33:20 +0530

    revert

---
+++ src/api/homerightbanner/services/homerightbanner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homerightbanner service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::homerightbanner.homerightbanner');
---
+++ src/api/hometenet/routes/hometenet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hometenet router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hometenet.hometenet');
