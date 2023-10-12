3511e163e025291b60df380e6126e5202be6084b
aecgdh <86180934+aecgdh@users.noreply.github.com>
2023-10-12 12:03:51 +0530

    12/10/2023

---
+++ src/api/blogs2/controllers/blogs2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blogs2 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blogs2.blogs2');
---
+++ src/api/blogs2/routes/blogs2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blogs2 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blogs2.blogs2');
