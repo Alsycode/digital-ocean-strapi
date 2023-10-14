6abe3ad2ecc783b58c87e763341d5282b474d599
aecgdh <86180934+aecgdh@users.noreply.github.com>
2023-10-14 15:45:39 +0530

    14/10/2023

---
+++ src/api/temple-list/controllers/temple-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * temple-list controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::temple-list.temple-list');
---
+++ src/api/temple-list/services/temple-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * temple-list service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::temple-list.temple-list');
