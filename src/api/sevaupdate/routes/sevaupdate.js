cd2d96848845561c490ba7d21f1cae4351f0161d
gdhadmin <gdhworkemail@gmail.com>
2023-10-24 12:09:31 +0530

    20/10/2023

---
+++ src/api/sevaupdate/routes/sevaupdate.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sevaupdate router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sevaupdate.sevaupdate');
---
+++ src/api/sevaupdate/services/sevaupdate.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sevaupdate service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sevaupdate.sevaupdate');
