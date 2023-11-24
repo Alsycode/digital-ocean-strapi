980387ba95030a694e39fb3c5e93241f16ab1ac2
gdhadmin <gdhworkemail@gmail.com>
2023-11-24 14:57:17 +0530

    service3

---
+++ src/api/testimonial3/controllers/testimonial3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * testimonial3 controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::testimonial3.testimonial3');
---
+++ src/api/testimonial3/routes/testimonial3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * testimonial3 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::testimonial3.testimonial3');
